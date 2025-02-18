29287a58f031af394a181b2dc9dc9692ab9e1315
Eliott Vacher Detournière <eliottvacher@gmail.com>
2025-02-18 15:54:28 +0100

    no

--- setup.py
+++ setup.py
@@ -3,13 +3,13 @@ from setuptools import setup, find_packages
 setup(
     name="tagterminal-arbitrage",
     version="0.1.0",
-    packages=find_packages(),
+    packages=find_packages(),  # Ensures 'tag-terminal' is included
     install_requires=[
         "google-cloud-storage",
     ],
     entry_points={
         "console_scripts": [
-            "list-gcs=tag-terminal.list_gcs_files:main",
+            "list-gcs=tag_terminal.list_gcs_files:main",
         ],
     },
 )
