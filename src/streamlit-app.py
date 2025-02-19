5cae46922e6a8b6f6c1887530ba7afbac9093dca
Eliott Vacher Detournière <eliottvacher@gmail.com>
2025-02-19 14:29:11 +0100

    yay

--- src/streamlit-app.py
+++
@@ -1,8 +0,0 @@
-import streamlit as st
-
-app2 = st.Page("app2.py", title="Arbitrage Finder", icon="💰")
-watchfinder_app = st.Page("watchfinder-app.py", title="Watchfinder.com Scraper", icon="⌚")
-
-pg = st.navigation([app2, watchfinder_app])
-st.set_page_config(page_title="Luxury Watch", page_icon="⌚")
-pg.run()
